8fa3feb298afee973d81c7dc2a8102c7ee947eb2
Lucas Verblud <lucasverblud@gmail.com>
2022-01-05 11:00:07 -0300

    name links navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,9 +5,9 @@ export default function NavBar() {
         <>
             <ul>
                 <li><a href="#">Home</a></li>
-                <li><a href="#">News</a></li>
-                <li><a href="#">Contact</a></li>
-                <li><a href="#">About</a></li>
+                <li><a href="#">Tienda</a></li>
+                <li><a href="#">Tabla de Talles</a></li>
+                <li><a href="#">Contacto</a></li>
             </ul>
         </>
     )
