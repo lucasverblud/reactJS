31caf404e8d99a41f200f18e02c42c8cf616ceca
Lucas Verblud <lucasverblud@gmail.com>
2022-01-25 23:53:33 -0300

    desafio7

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 
+const ItemList = [
+  { nombre: 'Inicio', enlace: '/' },
+  { nombre: 'Quienes Somos', enlace: '/QuienesSomos' },
+  { nombre: 'Contacto', enlace: '/contacto' },
+]
 
-const ItemList = [{ nombre: "Home", enlace: "/" }, { nombre: "Tienda", enlace: "/tienda" },
-{ nombre: "TablaTalles", enlace: "/tablaTalles" }, { nombre: "Contacto", enlace: "/contacto" }]
-
-export default function App() {
-
+function App() {
   return (
-    <>
+    <BrowserRouter>
       <NavBar data={ItemList} />
-      <ItemListContainer />
+      <Switch>
 
-    </>
-  );
+        <Route exact path="/">
+          <ItemListContainer />
+        </Route>
+
+        <Route path="/item/:itemId">
+          <ItemDetailContainer />
+        </Route>
+
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
+export default App;
 
---
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route, useParams, Link } from 'react-router-dom';
+import ItemDetail from "../ItemDetail/ItemDetail";
+
+
+export default function ItemDetailContainer() {
+
+    const { itemId } = useParams();
+    const [producto, setProducto] = useState({});
+
+    useEffect(() => {
+        setTimeout(() => {
+
+            let listadoDeProductos = [{ id: '001', title: 'Remera', price: 100, pictureUrl: '', stock: 5, initial: 1 },
+            { id: '002', title: 'Buzo', price: 50, pictureUrl: '', stock: 5, initial: 1 },
+            { id: '003', title: 'Campera', price: 70, pictureUrl: '', stock: 5, initial: 1 }];
+
+            listadoDeProductos = listadoDeProductos.filter(item => item.id == itemId);
+
+            let myProducto = listadoDeProductos[0];
+
+            setProducto(myProducto);
+
+        }, 2000)
+    }, [itemId])
+
+    return (
+        <>
+            {itemId}
+            <ItemDetail producto={producto} />
+        </>
+    );
+}
