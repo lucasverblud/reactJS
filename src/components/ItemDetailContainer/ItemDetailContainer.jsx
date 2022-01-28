754604324d003387a62016ff9e6ad3959d5d13ec
Lucas Verblud <lucasverblud@gmail.com>
2022-01-28 18:43:33 -0300

    correcciones1843

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,33 +1,54 @@
 import React, { useState, useEffect } from "react";
-import { useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import ItemDetail from "../ItemDetail/ItemDetail";
 
 
 export default function ItemDetailContainer() {
 
     const { itemId } = useParams();
+    const [Loading, setLoading] = useState(false);
     const [producto, setProducto] = useState({});
 
+    const productos = [
+        { id: '001', title: 'Remera1', price: 100, pictureUrl: "https://d22fxaf9t8d39k.cloudfront.net/6da0a68dbed7261cde30c9aa72c5483fcc40fe18c207a659d9140afc481ab8fb66621.jpeg", stock: 5, initial: 1, categoria: 'remera' },
+        { id: '002', title: 'Buzo1', price: 50, pictureUrl: "https://d22fxaf9t8d39k.cloudfront.net/c46bc4e954b3288e69bbdb808af37bf2d0811ea809711542c41445a92dfa664066621.jpeg", stock: 5, initial: 1, categoria: 'buzo' },
+        { id: '003', title: 'Hoodie1', price: 70, pictureUrl: "https://d22fxaf9t8d39k.cloudfront.net/9714f9d574809aeccf1fb2d0e1ec98a5ac02f888b01977398ee42f6bbb477de866621.jpeg", stock: 5, initial: 1, categoria: 'hoodie' }
+    ];
+
     useEffect(() => {
-        setTimeout(() => {
+        const productosEnStock = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                //reject('server caido');
+                resolve(productos);
+            }, 2000)
+        });
+        productosEnStock
+            .then(res => {
 
-            let listadoDeProductos = [
-                { id: '001', title: 'Remera1', price: 100, pictureUrl: "https://d22fxaf9t8d39k.cloudfront.net/6da0a68dbed7261cde30c9aa72c5483fcc40fe18c207a659d9140afc481ab8fb66621.jpeg", stock: 5, initial: 1, categoria: 'remera' },
-                { id: '002', title: 'Buzo1', price: 50, pictureUrl: "https://d22fxaf9t8d39k.cloudfront.net/c46bc4e954b3288e69bbdb808af37bf2d0811ea809711542c41445a92dfa664066621.jpeg", stock: 5, initial: 1, categoria: 'buzo' },
-                { id: '003', title: 'Hoodie1', price: 70, pictureUrl: "https://d22fxaf9t8d39k.cloudfront.net/9714f9d574809aeccf1fb2d0e1ec98a5ac02f888b01977398ee42f6bbb477de866621.jpeg", stock: 5, initial: 1, cagetoria: 'hoodie' }
-            ];
+                setLoading(true);
+                setProducto(res.find(item => item.id === itemId))
 
-            const myProducto = listadoDeProductos.find(item => item.id === itemId);
-
-            setProducto(myProducto);
-
-        }, 2000)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
     }, [itemId])
 
     return (
-        <>         
-            <ItemDetail producto={producto} />
-            {/* <Link to={"/item/002"}>ir al item 2</Link> */}
+
+        <>
+            {(Loading) ?
+                <>
+                    Loading...
+                </>
+                :
+                <ItemDetail producto={producto} />
+            }
+
         </>
-    );
+    )
+
 }
