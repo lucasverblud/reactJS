94a99246ca283e504525ecfbd8ae32565e82ce6e
Lucas Verblud <lucasverblud@gmail.com>
2022-01-31 08:49:07 -0300

    agrego bootstrap ItemDetail

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,19 +1,24 @@
 import React from "react";
-
+import { Container, Row, Col } from "react-bootstrap"
+import ItemCount from "../ItemCount/ItemCount";
 
 
 export default function ItemDetail({ producto }) {
 
     return (
         <>
-            <div>
-                {(producto.id) ?
-                    <>{producto.id + " " + producto.title + " " + producto.pictureUrl}</>
-                    :
-                    <>Loading...</>
-                }
-                {/* <ComponenteQueTenemos stock={producto.stock} id={producto.id}/> */}
-            </div>
+
+            <Container fluid className="contenedorProd">
+                <Row className="d-flex justify-content-center">
+                    <Col className="contenedorProdInv text-center col-md-6">
+                        <p className="fs-4 text-primary">{producto.title}</p>
+                        <p className="lead text-secondary text-uppercase">{producto.categoria}</p>
+                        <img alt='img' src={producto.pictureUrl} />
+                        <p className="lead"><span className="fw-bold">Precio: $ </span>{producto.price}</p>
+                        <ItemCount tope={producto.stock} ini={producto.initial} />
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 }
