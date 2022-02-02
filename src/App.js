d9e42cae9f9524f74965a84a6535fa1dd3f198b0
Lucas Verblud <lucasverblud@gmail.com>
2022-02-01 21:31:56 -0300

    desafio9 correciones

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
          </Route>
 
          <Route path="/cart">
-           <ItemDetailContainer />
+           {/* <ItemDetailContainer /> */}
          </Route>
 
        </Switch>
