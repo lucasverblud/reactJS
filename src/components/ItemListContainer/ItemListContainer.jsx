333f642e72ffcc08897ddea14cdd9ef5e4dd4655
Lucas Verblud <lucasverblud@gmail.com>
2022-02-03 19:42:36 -0300

    desafio10

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 import ItemList from "../ItemList/ItemList";
 import { useParams } from "react-router-dom";
+import Loader from "../Loader";
 
 export default function ItemListContainer() {
 
@@ -13,8 +14,9 @@ export default function ItemListContainer() {
         { id: '002', title: 'Buzo1', price: 50, pictureUrl: "https://d22fxaf9t8d39k.cloudfront.net/c46bc4e954b3288e69bbdb808af37bf2d0811ea809711542c41445a92dfa664066621.jpeg", stock: 5, initial: 1, categoria: 'buzo' },
         { id: '003', title: 'Hoodie1', price: 70, pictureUrl: "https://d22fxaf9t8d39k.cloudfront.net/9714f9d574809aeccf1fb2d0e1ec98a5ac02f888b01977398ee42f6bbb477de866621.jpeg", stock: 5, initial: 1, categoria: 'hoodie' }
     ];
-    
+
     useEffect(() => {
+        setLoading(true);
         const productosEnStock = new Promise((resolve, reject) => {
             setTimeout(() => {
                 //reject('server caido');
@@ -23,12 +25,12 @@ export default function ItemListContainer() {
         });
         productosEnStock
             .then(res => {
-                
-                setLoading(true);
+
                 if (categoriaId) {
                     setListItems(res.filter(item => item.categoria === categoriaId))
-                    
+
                 } else {
+
                     setListItems(res)
                 }
             })
@@ -38,18 +40,12 @@ export default function ItemListContainer() {
             .finally(() => {
                 setLoading(false);
             });
-    },[categoriaId])
+    }, [categoriaId])
 
     return (
 
         <>
-            {(Loading) ?
-                <>
-                    Loading...
-                </>
-                :
-                <ItemList items={listItems} />
-            }
+            {Loading ? <Loader /> : <ItemList items={listItems} />}
 
         </>
     )
