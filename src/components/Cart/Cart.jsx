5a23a788d97bc26a1ed544584180fc6b6913428b
Lucas Verblud <lucasverblud@gmail.com>
2022-02-15 22:26:14 -0300

    desafio13

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,16 +2,12 @@ import React, { useContext } from "react";
 import { contexto } from "../CartContext/CartContext";
 import { Link } from 'react-router-dom';
 import CartItem from "../CartItem/CartItem";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 const Cart = () => {
 
   const { cart, clear, sumarTodo } = useContext(contexto);
 
-  // const [total, setTotal] = useState(0);
-
-  // useEffect(() => {
-  //   setTotal(sumarTodo());
-  // }, [])
 
   return (
     <>
@@ -21,13 +17,21 @@ const Cart = () => {
           <Link to='/'>Ir al Inicio</Link>
         </div>
         :
-        <div>
+        <Container>
           {cart.map(element => <CartItem key={element.item.id} prod={element} />)}
-          Total ${sumarTodo()}
-          <button onClick={() => clear()}>Clear Cart</button>
-        </div>
+
+          <h4 className="my-3">Total ${sumarTodo()}</h4>
+
+          <Row className="mb-3">
+            <Col>
+              <Link to="/checkout" className="btn btn-success me-2">Finalizar Compra</Link>
+              <Button variant="danger" onClick={() => clear()}>Limpiar Carrito</Button>
+            </Col>
+          </Row>
+        </Container>
+
       }
-      
+
     </>
   )
 }
